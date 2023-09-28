20ee37bb6135daa24c7ad9cb4e30ce93159d74ee
Pavel Kazlou <obilonix@proton.me>
2023-09-28 12:43:56 +0300

    reinit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Specify your desired output directory here
+    outDir: 'build',
+  },
 })
+
